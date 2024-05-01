8c4b2dbe35c64cb09a6c58b096a869ee2a9d2acd
MOHAMMAD WASIF <erwasifmohd786@gmail.com>
2024-05-01 13:06:50 +0530

    Added Fragment

--- client/src/components/menu/Menu.jsx
+++ client/src/components/menu/Menu.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { PizzaApi } from "../../apis";
 import { Pizza } from "../index";
 
@@ -18,11 +18,14 @@ const Menu = () => {
     <main className="menu">
       <h2> Our menu</h2>
       {pizzaList.length ? (
+        <Fragment>
+          <p>Authentic italian cuisine. Some creative dishes to choose from. All from our stone oven, all orgainic, all delicious.</p>
         <ul className="pizzas">
           {pizzaList.map((pizza) => (
             <Pizza key={pizza.id} pizza={pizza} />
           ))}
         </ul>
+        </Fragment>
       ) : (
         <p>We're still working on menu. Please come back later :)</p>
       )}
